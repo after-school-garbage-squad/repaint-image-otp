c6421c5718c14e4ae84a329157f6211a3a2c8367
shiron <shiron4710.dev@gmail.com>
2023-10-19 09:51:54 +0900

    chore: rm debug

--- main.py
+++ main.py
@@ -124,4 +124,4 @@ def is_login():
         return jsonify({"message": "Authenticated"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
